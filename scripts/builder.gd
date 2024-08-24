750c0fb56fc7e897db90317c3288180ab47063c4
Jürgen Hackl <hackl@princeton.edu>
2024-08-24 16:58:02 -0400

    allow to add sprites

--- scripts/builder.gd
+++ scripts/builder.gd
@@ -3,14 +3,24 @@ extends Node3D
 # Load Selector Object
 @export var selector:Node3D
 @export var gridmap:GridMap
+@export var container:Node3D
+@export var sprites:Node3D
 
+@onready var seller = preload("res://sprites/seller.tscn")
+@onready var buyer = preload("res://sprites/buyer.tscn")
+
+# var agents:Array[PackedScene] = []
+
+@onready var agents = [buyer,seller]
 #@onready var selector = %Selector
 
 # Index of structure being built
-var index:int = 0 
+var index:int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+
+	preview()
 	pass # Replace with function body.
 
 
@@ -25,6 +35,10 @@ func _process(delta: float) -> void:
 	
 	# Toggle between agents
 	action_toggle()
+	
+	# Print Information
+	print(gridmap.get_cell_item(gridmap_position))
+	print(gridmap.map_to_local(gridmap_position))
 
 func action_add(gridmap_position:Vector3) -> void:
 	if Input.is_action_just_pressed("agent_add"):
@@ -32,15 +46,35 @@ func action_add(gridmap_position:Vector3) -> void:
 		gridmap.set_cell_item(gridmap_position, index, 
 			gridmap.get_orthogonal_index_from_basis(selector.basis))
 		
+		var _agent = agents[index].instantiate()
+		
+		# Correction term to fix cell offset
+		var _correction = gridmap.cell_size/2
+		_correction.y = 0
+		_agent.global_position = gridmap.map_to_local(gridmap_position) - _correction
+
+		sprites.add_child(_agent)
+		
 func action_remove(gridmap_position:Vector3) -> void:
 	if Input.is_action_just_pressed("agent_remove"):
 		gridmap.set_cell_item(gridmap_position, -1)
 
 func action_toggle() -> void:
-	var tile_size:int = gridmap.mesh_library.get_item_list().size()
-	print(tile_size)
+	var tile_size = gridmap.mesh_library.get_item_list().size()
+
 	if Input.is_action_just_pressed("agent_next"):
-		index = wrap(index + 1, 0, 2)
+		index = wrap(index + 1, 0, tile_size)
 	
 	if Input.is_action_just_pressed("agent_previous"):
-		index = wrap(index - 1, 0, 2)
+		index = wrap(index - 1, 0, tile_size)
+		
+	preview()
+
+func preview() -> void:
+	# Clear preview container
+	for n in container.get_children():
+		container.remove_child(n)
+		
+	# Create new preveiw
+	var _agent = agents[index].instantiate()
+	container.add_child(_agent)
