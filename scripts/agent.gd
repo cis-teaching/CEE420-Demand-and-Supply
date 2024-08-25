c65c4225d877c979ab6fef47d78803c0f7949e47
Jürgen Hackl <hackl@princeton.edu>
2024-08-25 08:42:22 -0400

    add functions to the agent class

--- scripts/agent.gd
+++ scripts/agent.gd
@@ -2,9 +2,11 @@ extends Node3D
 
 class_name AgentClass
 
-
-
+# Class varialbes
 var limit:float=0.0
+var _initial:float=INF
+var _prices:Array[float]=[_initial]
+var success:bool=false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,6 +16,37 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func get_price()->float:
+	return _prices[-1]
+	
+func set_price(price:float)->void:
+	_prices.append(price)
+	success=true
+	
+func get_initial()->float:
+	return _initial
+	
+func set_initial(price:float)->void:
+	_initial = price
+	_prices.append(price)
+	
+func adjust_price()->void:
+	pass
+	
+func get_adjustment(kind:String="fixed")->float:
+	var adjustment:float = 1
+	
+	if kind == "random":
+		var random = RandomNumberGenerator.new()
+		random.randomize()
+		if random.randf() < 0.5:
+			adjustment = 0
+			
+	elif kind == "function":
+		adjustment = (0.2 * abs(self.get_price()-limit) +1) ** 0.5
+		
+	return adjustment
 
 func add() -> void:
 	pass
